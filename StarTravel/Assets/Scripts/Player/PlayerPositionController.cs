27ad98daeda55cc8ad63abc01b84df1f960beb02
KrSergei <34679066+KrSergei@users.noreply.github.com>
2023-08-15 15:27:37 +0300

    added cinemashine

---
+++ StarTravel/Assets/Scripts/Player/PlayerPositionController.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class PlayerPositionController : MonoBehaviour
+{
+
+   
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+             
+    }
+}
