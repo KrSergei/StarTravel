db33519b2b15c153a30f70298d747289f4cdcdf8
KrSergei <34679066+KrSergei@users.noreply.github.com>
2024-01-15 22:00:06 +0300

    refactoring

--- StarTravel/Assets/Scripts/Player/PlayerController.cs
+++ StarTravel/Assets/Scripts/Player/PlayerController.cs
@@ -1,22 +1,20 @@
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private FixedJoystick _playerJoystick;
-    [SerializeField] private CharacterController _playerCC;
+    [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _moveSpeed;
     
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         if (_playerJoystick.Horizontal != 0 || _playerJoystick.Vertical != 0)
-            _playerCC.Move(new Vector2(_playerJoystick.Horizontal * _moveSpeed, _playerJoystick.Vertical * _moveSpeed) * Time.deltaTime);
-            
+            _rb.AddForce(new Vector2(_playerJoystick.Horizontal, _playerJoystick.Vertical) * _moveSpeed * Time.fixedDeltaTime);
     }
 }
